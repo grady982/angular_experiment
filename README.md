470864b2239369ec09cd8e293b5f1f3dde5f7ccd
grady982 <grady0902@gmail.com>
2022-02-08 14:47:04 +0800

    test markdown link image

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+![test](/src/assets/img/test.svg "test")
+
 # AngularExperiment
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.9.
---
+++ apollo.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    client: {
+        services: {
+            name: 'angular-spacex-graphql-codegen',
+            url: 'https://api.spacex.land/graphql/'
+        }
+    }
+}
